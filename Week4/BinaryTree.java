39b88689425264b08feab7fcc044a14c469e3453
Winsten Coellins <62731781+winstencoellins@users.noreply.github.com>
2021-08-22 12:39:06 +0700

    Add files via upload

---
+++ Week4/BinaryTree.java
@@ -0,0 +1,18 @@
+
+public class BinaryTree {
+	
+	/*
+	 * Inserting a value into a binary tree
+	 */
+	public void insert(Node value) {
+		// TODO: Implement the insert method
+	}
+	
+	/*
+	 * Searching a value in a binary tree
+	 * @return true if the value exists, else otherwise
+	 */
+	public boolean search(Node value) {
+		return true;
+	}
+}
---
+++ Week4/Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+	private int value;
+	private Node right;
+	private Node left;
+	
+	public Node(int value) {
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+	
+}
